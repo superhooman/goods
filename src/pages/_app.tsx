fe55a0f75c260fee42cba25a982009c832f7a9eb
Dasha Taratynova <uenify@gmail.com>
2023-02-02 21:28:24 +0600

    feat: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import { Analytics } from '@vercel/analytics/react';
 
 import { api } from "@src/utils/api";
 
@@ -17,6 +17,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <Component {...pageProps} />
       <Toaster />
+      <Analytics />
     </SessionProvider>
   );
 };
